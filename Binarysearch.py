317ec633fc4e15dae4bafd8c8a0eb44f4e497bd1
SABARIRAJKUMAR <87665718+SABARIRAJKUMAR@users.noreply.github.com>
2021-10-26 19:50:06 +0530

    Update Binarysearch.py

--- Binarysearch.py
+++ Binarysearch.py
@@ -4,30 +4,25 @@ def binary_search(list1, n):
     mid = 0  
   
     while low <= high:  
-        # for get integer result   
+         
         mid = (high + low) // 2  
-  
-        # Check if n is present at mid   
+     
         if list1[mid] < n:  
             low = mid + 1  
   
-        # If n is greater, compare to the right of mid   
+   
         elif list1[mid] > n:  
             high = mid - 1  
-  
-        # If n is smaller, compared to the left of mid  
+ 
         else:  
             return mid  
-  
-            # element was not present in the list, return -1  
+   
     return -1  
   
-  
-# Initial list1  
+    
 list1 = [12, 24, 32, 39, 45, 50, 54]  
 n = 45  
-  
-# Function call   
+    
 result = binary_search(list1, n)  
   
 if result != -1:  
